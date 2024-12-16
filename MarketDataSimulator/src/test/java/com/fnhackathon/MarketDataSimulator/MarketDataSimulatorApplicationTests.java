0f98cf2619efd07fcad0220d7404f01414417570
harshajayanth-tech <harsha.mulaparthi@finvedic.com>
2024-12-16 22:43:31 +0530

    Spring Repository Commit

---
+++ MarketDataSimulator/src/test/java/com/fnhackathon/MarketDataSimulator/MarketDataSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fnhackathon.MarketDataSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
